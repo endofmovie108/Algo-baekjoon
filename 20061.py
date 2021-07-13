1334b0092ac22664e0e4727668392e2f631408a7
endofmovie108 <endofmovie108@gmail.com>
2021-07-14 01:07:12 +0900

    baekjoon

--- 20061.py
+++ 20061.py
@@ -5,101 +5,148 @@ BLOCK_1 = 2
 BLOCK_2 = 3
 N = int(input())
 X, Y = 10, 10
-# x: 행 , y: 열
+# x: 행 , y: 열A
+GRN = 0
+BLU = 1
+maps = [[0]*Y for _ in range(X)]
+dx = [1, 0]
+dy = [0, 1]
 
-def inMap(x, y):
+def outMaps(x, y):
     global X, Y
-    if 0<=x<X and 0<=y<Y: return True
-    else: return False
+    if 0<=x<X and 0<=y<Y: return False
+    else: return True
 
 def meetBlock(x, y, maps):
     if maps[x][y] == 1: return True
     else: return False
 
-def inGreenBorder(num_block, xs):
-    cnt = 0
-    for i_b in range(num_block):
-        x = xs[i_b]
-        if 4<=x<6: cnt+=1
-    return cnt, True if cnt>0 else False
+def onlyUpdate(n_blocks, xs, ys, maps):
+    for i in range(n_blocks):
+        maps[xs[i]][ys[i]] = 1
 
-def setBlockToMap(xs, ys, maps):
-    for i_b in range(num_block):
-        maps[xs[i_b]][ys[i_b]] = 1
-
-def mover_down(num_block, xs, ys, maps):
-    global X, Y
-    MOVE_END = False
-    while not MOVE_END:
-
-        flag_STOP = False
-        for i_b in range(num_block):
-            xt, yt = xs[i_b] + 1, ys[i_b]
-
-            if not inMap(xt, yt):
-                MOVER_STOP_CODE = 0 # 하나라도 맵 외부로 나갔을 때
-                flag_STOP = True
+def oneLineFull(maps, CLR):
+    list_oneLineFull = []
+    for i in range(4, 10):
+        for j in range(4):
+            if CLR == GRN and maps[i][j] == 0:
                 break
-
-            if meetBlock(xt, yt, maps):
-                MOVER_STOP_CODE = 1 # 하나라도 이미 놓여진 block을 만났을 때
-                flag_STOP = True
+            elif CLR == BLU and maps[j][i] == 0:
                 break
+        else: # 전부 1이여서 full line일 때
+            list_oneLineFull.append(i)
+    return list_oneLineFull
 
-        if flag_STOP:
-            if MOVER_STOP_CODE == 1: # 중단되었으며 경계에 있는 경우
-                numRowGrnBrd, inGrnBrdFlg = inGreenBorder(num_block, xs)
-                if (inGrnBrdFlg is False) or (numRowGrnBrd == 0): print("green border error!")
-                # numRowGrnBrd 만큼 아래로 이동
-                for r_idx in range(X-1-numRowGrnBrd, 3, -1):
-                    for c_idx in range(4):
-                        maps[r_idx+numRowGrnBrd][c_idx] = maps[r_idx][c_idx]
-                        if 4<=r_idx<6: maps[r_idx][c_idx] = 0
-                MOVE_END = True
-                break
+def pullAndUpdate(list_olf, maps, CLR):
+    for olf in list_olf:
+        for i in range(olf, 4, -1):
+            for j in range(4):
+                if CLR == GRN:
+                    maps[i][j] = maps[i-1][j]
+                    maps[i-1][j] = 0
+                elif CLR == BLU:
+                    maps[j][i] = maps[j][i-1]
+                    maps[j][i-1] = 0
 
-            elif MOVER_STOP_CODE == 0:
-                # 중단되었으며 maps를 벗어난 경우, do nothing
-                MOVE_END = True
-                break
+def pushAndPullUpdate(n_inBorder, maps, CLR):
+    for i in range(10-1-n_inBorder, 4, -1):
+        for j in range(4):
+            if CLR == GRN:
+                maps[i+n_inBorder][j] = maps[i][j]
+                maps[i][j] = 0
+            elif CLR == BLU:
+                maps[j][i+n_inBorder] = maps[j][i]
+                maps[j][i] = 0
 
-        else: # 정상적으로 block이 이동된 경우
-            # 리스트 갱신 및 맵 반영
-            for i_b in range(num_block):
-                xs[i_b], ys[i_b] = xs[i_b] + 1, ys[i_b]
-            setBlockToMap(xs, ys, maps)
 
 def print_maps(maps):
     global X, Y
-    for r in range(X):
-        for c in range(Y):
-            print(maps[r][c], end=' ')
+    for i in range(X):
+        for j in range(Y):
+            print(maps[i][j], end=' ')
         print()
+    print()
 
-def SEQ_MOVING(num_block, xs, ys, maps):
-    # down, GREEN area
-    mover_down(num_block, xs, ys, maps)
-    # right, BLUE area
-    #mover_right(num_block, xs, ys, maps)
+def isBlockInBorder(maps, CLR):
+    cnt = 0
+    for i in range(4, 6):
+        for j in range(4):
+            if (CLR == GRN and maps[i][j] == 1) or (CLR == BLU and maps[j][i] == 1):
+                cnt += 1
+                break
+    return cnt
 
-maps = [[0] for _ in range(10)]
+def Mover(n_blocks, xs_ori, ys_ori, maps, CLR):
+    xs = xs_ori.copy()
+    ys = ys_ori.copy()
+    score = 0
+    STOP_FLAG = False
+    while not STOP_FLAG:
+        for i in range(n_blocks):
+            xt, yt = xs[i] + dx[CLR], ys[i] + dy[CLR]
+            if outMaps(xt, yt):
+                STOP_FLAG = True
+                break
+            if meetBlock(xt, yt, maps):
+                STOP_FLAG = True
+                break
+        else:
+            # break 걸리지 않고 넘어왔으므로, update
+            for i in range(n_blocks):
+                xs[i], ys[i] = xs[i] + dx[CLR], ys[i] + dy[CLR]
+    # 만약 STOP_FLAG == True 로 탈출했다면
+    # 해당 상자의 이동이 끝난 것 이므로 한 줄이 전부 차있는 것이 있는지 check
+    # maps에 업데이트
+    onlyUpdate(n_blocks, xs, ys, maps)
+    
+    # 만약 one line full 인 라인이 있다면
+    list_oneLinFull = oneLineFull(maps, CLR)
+    if len(list_oneLinFull):
+        score += len(list_oneLinFull)
+        pullAndUpdate(list_oneLinFull, maps, CLR)
+
+
+    # 만약 경계안에 block이 존재한다면
+    n_inBorder = isBlockInBorder(maps, CLR)
+    if n_inBorder:
+        pushAndPullUpdate(n_inBorder, maps, CLR)
+    return score
+
+def Area_score(maps, CLR):
+    score = 0
+    for i in range(4, 10):
+        for j in range(4):
+            if CLR == GRN: score += maps[i][j]
+            elif CLR == BLU: score += maps[j][i]
+    return score
+
+
+total_score = 0
 for i in range(N):
     t, x, y = map(int, input().split())
-
-    xs = []
-    ys = []
-    xs.append(x)
-    ys.append(y)
+    xs, ys = [x], [y]
     if t == BLOCK_0:
-        num_block = 1
+        n_blocks = 1
     elif t == BLOCK_1:
-        num_block = 2
+        n_blocks = 2
         xs.append(x)
         ys.append(y+1)
     elif t == BLOCK_2:
-        num_block = 2
+        n_blocks = 2
         xs.append(x+1)
         ys.append(y)
 
-    SEQ_MOVING(num_block, xs, ys, maps)
+
+    total_score += Mover(n_blocks, xs, ys, maps, GRN)
+    total_score += Mover(n_blocks, xs, ys, maps, BLU)
     print_maps(maps)
+
+grn_score = Area_score(maps, GRN)
+blu_score = Area_score(maps, BLU)
+
+print(total_score)
+print(grn_score+blu_score)
+
+
+
+
