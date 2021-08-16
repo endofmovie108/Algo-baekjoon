6aec10532e687d45c06001d09a019af46f47fda0
endofmovie108 <endofmovie108@gmail.com>
2021-08-16 21:10:29 +0900

    16236 BFS

--- 16236.py
+++ 16236.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque as dq
 sys.stdin = open('input.txt', 'rt')
 N = int(input())
 maps = [list(map(int, input().split())) for _ in range(N)]
@@ -20,31 +21,43 @@ def isSharkCanEat(r, c, s):
     else:
         return False
 
-d
+def BFS(shark_r, shark_c, shark_s, chk):
+    step = 0
+    find_step = 0
+    shark_dq = dq([[shark_r, shark_c, step]])
+    chk[shark_r][shark_c] = 1
+    find_flag = False
+    res_list = []
+    while shark_dq:
+        [r, c, step] = shark_dq.popleft()
 
-def DFS(r, c, s, rc_new, chk, lv):
-    if not isSharkCanGo(r, c, s, chk):
-        return
+        if isSharkCanEat(r, c, shark_s):
+            find_flag = True
+            find_step = step
+            res_list.append([r, c, step])
 
-    if isSharkCanEat(r, c, s):
-        rc_new.append([r, c, lv, 0])
+        # find_step < step 조건:
+        # BFS 특성상, 해당 조건으로 다음 계층의 노드를 탐색중임을 알 수 있음
+        # 다음 계층의 노드를 탐색중인것은 거리가 더 물고기들을 탐색중인 것 이므로
+        # return 해야할 필요가 있다.
+        if find_flag and find_step < step:
+            return res_list
 
-    for i in range(4):
-        DFS(r+dr[i], c+dc[i], s, rc_new, chk, lv+1)
+        for i in range(4):
+            rt, ct = r+dr[i], c+dc[i]
+            if isSharkCanGo(rt, ct, shark_s, chk):
+                shark_dq.append([rt, ct, step+1])
+    return res_list
 
-def decideWhatFish(rc_new, shark_r, shark_c, shark_s, shark_e):
-    global maps
-    for i in range(len(rc_new)):
-        rc_new[i][3] = abs(shark_r - rc_new[i][0]) + abs(shark_c - rc_new[i][1])
-
-    rc_new.sort(key=lambda x: (x[3], x[0], x[1]))
-    shark_r, shark_c = rc_new[0][0], rc_new[0][1]
-    maps[shark_r][shark_c] = 0
+def decideWhatFish(res_list, shark_s, shark_e):
+    res_list.sort(key=lambda x:(x[2], x[0], x[1]))
+    shark_r, shark_c, shark_step = res_list[0][0], res_list[0][1], res_list[0][2]
     shark_e += 1
     if shark_e >= shark_s:
         shark_s += 1
         shark_e = 0
-    return shark_r, shark_c, shark_s, shark_e
+    maps[shark_r][shark_c] = 0
+    return shark_r, shark_c, shark_step, shark_s, shark_e
 
 # Main
 # 1. find shark
@@ -60,14 +73,13 @@ cnt = 0
 while True:
     shark_r_prev, shark_c_prev = shark_r, shark_c
     chk = [[0]*N for _ in range(N)]
-    rc_new = [] # row, col, level
-    DFS(shark_r, shark_c, shark_s, rc_new, chk, 0)
-    cnt += 1
 
-    if not len(rc_new) > 0:
+    res_list = BFS(shark_r, shark_c, shark_s, chk)
+
+    if len(res_list) == 0:
         print(cnt)
         break
     else:
-        shark_r, shark_c, shark_s, shark_e = decideWhatFish(rc_new, shark_r, shark_c, shark_s, shark_e)
-
+        shark_r, shark_c, step, shark_s, shark_e = decideWhatFish(res_list, shark_s, shark_e)
+        cnt += step
 
