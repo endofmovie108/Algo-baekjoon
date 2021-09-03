72fe8ce20e5c50162b2bae2883efafbd4b529418
endofmovie108 <endofmovie108@gmail.com>
2021-09-03 15:20:26 +0900

    19237 failed

--- 19237.py
+++ 19237.py
@@ -4,82 +4,142 @@ sys.stdin = open('input.txt', 'rt')
 
 N, M, K = map(int, input().split())
 shark_maps = [list(map(int, input().split())) for _ in range(N)]
+#shark_maps = [list(map(int, input())) for _ in range(N)]
 shark_dirs = list(map(int, input().split()))
 shark_dir_priors = [list(map(int, input().split())) for _ in range(4*M)]
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-# smell maps
-# [[ori_idx, time], [], [], ...]
-# []
-# []
-smell_maps = []
-for r in range(N):
-    smell_maps_sub = []
-    for c in range(N):
-        smell_maps_sub.append([])
-    smell_maps.append(smell_maps_sub)
+# shark maps
+# [[shark_idx, 0, 0, 0, ...]]
+# [[0, 0, 0, ...]]
 
-# smell stats deque
-# [[ori_idx, r, c, time], [], ...]
-smell_stats_dq = dq([])
-
-# find shark, save to status deque
-# [
-#  [ori_idx, row, col, dir],
-#  [],
-#  []
-# ]
-shark_stats_dq = dq([list() for _ in range(M)])
+# shark stats
+# [[shark_idx, r, c, d], ...]
+shark_stats = [[0, 0, 0, 0]]*M
 for r in range(N):
     for c in range(N):
-        shark_idx_ori = shark_maps[r][c]
-        if shark_idx_ori != 0:
-            shark_stats_dq[shark_idx_ori - 1].append(shark_idx_ori)
-            shark_stats_dq[shark_idx_ori - 1].append(r)
-            shark_stats_dq[shark_idx_ori - 1].append(c)
-            shark_stats_dq[shark_idx_ori - 1].append(shark_dirs[shark_idx_ori - 1])
+        shark_idx = shark_maps[r][c]
+        if shark_idx != 0:
+            shark_stats[shark_idx-1] = [shark_idx, r, c, shark_dirs[shark_idx-1]]
 
-def isInMaps(r, c):
-    return 0<=r<N and 0<=c<N
+# smell_maps
+# [[shark_idx, time], [0, 0], [0, 0], ...]
+# [[0, 0], [0, 0], ...]
+# [[0, 0], [0, 0], ...]
+smell_maps = [[[0, 0]]*N for _ in range(N)]
 
-def isNoSmell(r, c):
-    return len(smell_maps[r][c]) == 0
+# smell_stats
+# [[[shark_idx, r, c, time]],
+#  [[0, 0, 0, 0]],
+#  [[0, 0, 0, 0]], ...]
+smell_stats = [[[0, 0, 0, 0]]]*M
 
-def retDir(shark_idx_ori, dir):
-    return shark_dir_priors[4*(shark_idx_ori-1)+dir-1]
+def spreadSmells(shark_stats, smell_maps, smell_stats):
+    # Old smell
+    # deque를 copy로 삭제해야할 값을 찾고,
+    # time 이 0이 되거나 음수인 경우 해당 index의 deque를 del()로 삭제
+    for smell_stat in smell_stats:
+        del_cnt = 0
+        for sub_idx, smell_stat_sub in enumerate(smell_stat[0].copy()):
+            sub_idx -= del_cnt
+            [shark_idx, r, c, time] = smell_stat_sub
+            time -= 1
+            smell_stat_sub = [shark_idx, r, c, time]
+            if time < 1:
+                del(smell_stat[0][sub_idx])
+                smell_maps[r][c] = [0, 0]
+                del_cnt += 1
+            else:
+                smell_stat[0][sub_idx] = smell_stat_sub
+                smell_maps[r][c] = [shark_idx, time]
 
-# Main loop
-while True:
-    # 0. spread smell
-    for i in range(len(shark_stats_dq)):
-        shark_stat = shark_stats_dq.popleft()
-        [shark_idx_ori, row, col, dir] = shark_stat
-        # 0-1. mark to smell_maps
-        smell_maps[row][col] = [shark_idx_ori, K]
-        # 0-2. update stats
-        smell_stats_dq.append([shark_idx_ori, row, col, K])
-        shark_stats_dq.append(shark_stat)
+    # New smell
+    for shark_idx in range(1, M+1):
+        shark_stat = shark_stats[shark_idx-1]
+        [_, r, c, _] = shark_stat
 
-    # 1. move shark
-    for i in range(len(shark_stats_dq)):
-        shark_stat = shark_stats_dq.popleft()
-        [shark_idx_ori, row, col, dir] = shark_stat
+        # update smell_maps
+        smell_maps[r][c] = [shark_idx, K]
 
-        # 1-1. find non smell area
-        cnt = 0
-        for j in range(4):
-            dir_s = retDir(shark_idx_ori, dir)
-            ddir = dir_s[j]
-            row_t, col_t = row + dr[ddir-1], col + dc[ddir-1]
-            if isInMaps(row_t, col_t) and isNoSmell(row_t, col_t):
-                row_r, col_r = row_t, col_t
+        # update smell_stats
+        if len(smell_stats[shark_idx-1]) == 0:
+            smell_stats[shark_idx-1] = [[shark_idx, r, c, K]]
+        else:
+            smell_stats[shark_idx-1][0].append([shark_idx, r, c, K])
+
+def returnDir(shark_dir_priors, shark_idx, dir, i):
+    return shark_dir_priors[4*(shark_idx-1) + (dir-1)][i]-1
+
+def isNoSmell(r, c, smell_maps):
+    return 0<=r<N and 0<=c<N and smell_maps[r][c][0] == 0
+
+def isMySmell(r, c, shark_idx, smell_maps):
+    return 0<=r<N and 0<=c<N and smell_maps[r][c][0] == shark_idx
+
+def moveShark(r, c, rt, ct, shark_idx, shark_maps, shark_stats, ii):
+    # update shark_stats
+    shark_stats[shark_idx-1] = [shark_idx, rt, ct, ii+1]
+    # update shark_maps
+    shark_maps[r][c] = 0
+    shark_maps[rt][ct] = shark_idx
+
+def moveSharks(shark_stats, shark_maps, smell_maps):
+    for shark_idx in range(1, M+1):
+        # cond 1: 아무 냄새가 안나는 곳
+        shark_stat = shark_stats[shark_idx-1]
+        [shark_idx_tmp, r, c, dir] = shark_stat
+        if shark_idx_tmp == 0: continue
+        for i in range(4):
+            ii = returnDir(shark_dir_priors, shark_idx, dir, i)
+            rt, ct = r+dr[ii], c+dc[ii]
+            if isNoSmell(rt, ct, smell_maps):
+                moveShark(r, c, rt, ct, shark_idx, shark_maps, shark_stats, ii)
                 break
         else:
-            # 1-2. go back to
+            # cond 2: 자신의 냄새로 back
+            for i in range(4):
+                ii = returnDir(shark_dir_priors, shark_idx, dir, i)
+                rt, ct = r+dr[ii], c+dc[ii]
+                if isMySmell(rt, ct, shark_idx, smell_maps):
+                    moveShark(r, c, rt, ct, shark_idx, shark_maps, shark_stats, ii)
+                    break
 
+    for shark_idx1 in range(1, M):
+        shark_stat1 = shark_stats[shark_idx1-1]
+        [_, r1, c1, _] = shark_stat1
+        for shark_idx2 in range(shark_idx1+1, M+1):
+            shark_stat2 = shark_stats[shark_idx2-1]
+            [_, r2, c2, _] = shark_stat2
+            if r1 == r2 and c1 == c2:
+                # shark_idx1 is lower than shark_idx2!
+                # delete shark2
+                shark_stats[shark_idx2-1] = [0, 0, 0, 0]
+                shark_maps[r2][c2] = shark_idx1
 
+def print_sharkMaps(cnt):
+    print(cnt)
+    for r in range(N):
+        print(shark_maps[r])
+    print()
 
-        shark_stats_dq.append(shark_stat)
+    for r in range(N):
+        print(smell_maps[r])
+    print()
+    print()
+
+cnt = 0
+spreadSmells(shark_stats, smell_maps, smell_stats)
+print_sharkMaps(cnt)
+while True:
+    cnt += 1
+
+    # 1. Move sharks
+    moveSharks(shark_stats, shark_maps, smell_maps)
+
+    # 0. Spread smells
+    spreadSmells(shark_stats, smell_maps, smell_stats)
+
+    print_sharkMaps(cnt)
 
