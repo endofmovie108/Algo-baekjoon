47398a5db4fa46251dc73d66f0e0f5cf5f6a8aa5
endofmovie108 <endofmovie108@gmail.com>
2021-09-22 01:13:21 +0900

    19236 DFS success

--- 19236-DFS.py
+++ 19236-DFS.py
@@ -1,128 +1,140 @@
 import sys
-from copy import deepcopy
+import copy
 sys.stdin = open('input.txt', 'rt')
 
-maps = [list(map(int, input().split())) for _ in range(4)]
-print(maps)
-
-# up, left-up, left, left-down, down, right-down, right, right-up
-dr = [-1, -1, 0, 1, 1, 1, 0, -1]
-dc = [0, -1, -1, -1, 0, 1, 1, 1]
-
-def isInMaps(row, col):
-    return 0 <= row < 4 and 0 <= col < 4
-
-def isFishIdx(maps, row, col):
-    return 1 <= maps[row][col] <= 16
-
-def isSharkCanEatAndMove(maps, row, col):
-    return isInMaps(row, col) and isFishIdx(maps, row, col)
-
-def moveShark(maps, row, col, fishStats_dict, sharkStat):
-    fish_idx = maps[row][col]
-    [fish_row, fish_col, fish_dir] = fishStats_dict[fish_idx]
-    if fish_row != row or fish_col != col:
-        print("ERR!!!")
-
-    # erase prev shark location
-    [row_old, col_old, dir_old] = sharkStat
-    maps[row_old][col_old] = 0
-
-    # apply to maps, sharkStat
-    maps[row][col] = -1
-    sharkStat = [row, col, fish_dir]
-
-    # delete fish of fishStats_dict
-    del(fishStats_dict[fish_idx])
-    return maps, sharkStat
-
-def isFishCanMoveThenMove(maps, fish_idx_1, fish_row_2, fish_col_2, fish_dir_1, fishStats_dict):
-    # fish 1의 이동 전 row, col
-    [fish_row_1, fish_col_1, _] = fishStats_dict[fish_idx_1]
-
-    if isInMaps(fish_row_2, fish_col_2): # fish 1의 이동 후 row, col
-        if isFishIdx(maps, fish_row_2, fish_col_2): # fish거나 -> 움직임
-            fish_idx_2 = maps[fish_row_2][fish_col_2]
-            [_, _, fish_dir_2] = fishStats_dict[fish_idx_2]
-
-            fishStats_dict[fish_idx_2] = [fish_row_1, fish_col_1, fish_dir_2]
-            fishStats_dict[fish_idx_1] = [fish_row_2, fish_col_2, fish_dir_1] # direction은 그대로 가져감
-
-            maps[fish_row_1][fish_col_1] = fish_idx_2
-            maps[fish_row_2][fish_col_2] = fish_idx_1
-
-            return True, maps, fishStats_dict
-
-        elif maps[fish_row_2][fish_col_2] == 0: # 빈칸이거나 -> 움직임
-
-            maps[fish_row_1][fish_col_1] = 0
-            maps[fish_row_2][fish_col_2] = fish_idx_1
-            fishStats_dict[fish_idx_1] = [fish_row_2, fish_col_2, fish_dir_1]
-
-            return True, maps, fishStats_dict
-        else: # 상어인 경우
-            return False, maps, fishStats_dict
-    else:
-        return False, maps, fishStats_dict
+# define global variables
+DR = [-1, -1, 0, 1, 1, 1, 0, -1] # up, up-left, left, down-left, down, down-right, right, up-right
+DC = [0, -1, -1, -1, 0, 1, 1, 1]
+MAP_SIZE = 4
+DIR_NUM = 8
+MAX_EATIDXSUM = -1
 
 
-def playSharkDFS(maps, fishStats_dict, sharkStat, level, eat_sum):
-    print(level, eat_sum)
-    # 물고기들의 움직임
-    for fish_idx, [fish_row, fish_col, fish_dir] in fishStats_dict.items():
+# set maps, fishStats
+maps = [list(map(int, input().split())) for _ in range(MAP_SIZE)]
+fishStats_dict = dict()
+for row in range(MAP_SIZE):
+    for col in range(MAP_SIZE):
+        fishIdx = maps[row][2*col+0]
+        fishDir = maps[row][2*col+1]-1
+        fishStats_dict[fishIdx] = [row, col, fishDir]
+mapsLegacy = maps.copy()
+maps = [[0]*MAP_SIZE for _ in range(MAP_SIZE)]
+for row in range(MAP_SIZE):
+    for col in range(MAP_SIZE):
+        maps[row][col] = mapsLegacy[row][2*col+0] # remain fish index only
+del(mapsLegacy)
 
-        for dir_idx in range(8):
-            fish_dir_t = (fish_dir + dir_idx) % 8
-            fish_row_t = fish_row + dr[fish_dir_t]
-            fish_col_t = fish_col + dc[fish_dir_t]
-            fishMoveSuccess, maps, fishStats_dict = isFishCanMoveThenMove(maps, fish_idx, fish_row_t, fish_col_t, fish_dir_t, fishStats_dict)
-            if fishMoveSuccess: break
-    # 상어가 갈 수 있는곳을 찾기
+# set shark status, maps
+def sharkEatFish(sharkStat, tgt_row, tgt_col, maps, fishStats_dict, eatIdxSum):
     [shark_row, shark_col, shark_dir] = sharkStat
-    shark_dir_t = shark_dir
-    # for dir_idx in range(8):
-    #     shark_dir_t = (shark_dir + dir_idx) % 8
-    #     leastOneFlag = False
+
+    # maps
+    fishIdx = maps[tgt_row][tgt_col]
+    maps[shark_row][shark_col] = 0
+    maps[tgt_row][tgt_col] = -1
+
+    # stat
+    [fish_row, fish_col, fish_dir] = fishStats_dict[fishIdx]
+    del(fishStats_dict[fishIdx])
+    if fish_row != tgt_row or fish_col != tgt_col: print('ERR! sharkEatFish()')
+    sharkStat = [fish_row, fish_col, fish_dir]
+    return maps, sharkStat, fishStats_dict, eatIdxSum+fishIdx
+
+sharkStat = [0, 0, 0]
+eatIdxSum = 0
+maps, sharkStat, fishStats_dict, eatIdxSum = sharkEatFish(sharkStat, 0, 0, maps, fishStats_dict, eatIdxSum)
+
+def isInMaps(r, c):
+    return 0<=r<MAP_SIZE and 0<=c<MAP_SIZE
+
+def isShark(maps, r, c):
+    return maps[r][c] == -1
+
+def isEmpty(maps, r, c):
+    return maps[r][c] == 0
+
+def isFish(maps, r, c):
+    return 1<=maps[r][c]<=16
+
+def moveFishes(maps, fishStats_dict):
+    for fishIdx in sorted(list(fishStats_dict.keys())):
+        [fish_row, fish_col, fish_dir] = fishStats_dict[fishIdx]
+        for dirIdx in range(8):
+            fish_dir_tmp = (fish_dir+dirIdx)%8
+            fish_row_tmp = fish_row + DR[fish_dir_tmp]
+            fish_col_tmp = fish_col + DC[fish_dir_tmp]
+            if (not isInMaps(fish_row_tmp, fish_col_tmp)) or isShark(maps, fish_row_tmp, fish_col_tmp):
+                continue
+            elif isEmpty(maps, fish_row_tmp, fish_col_tmp):
+                maps[fish_row_tmp][fish_col_tmp] = fishIdx
+                maps[fish_row][fish_col] = 0
+                fishStats_dict[fishIdx] = [fish_row_tmp, fish_col_tmp, fish_dir_tmp]
+                break
+            elif isFish(maps, fish_row_tmp, fish_col_tmp):
+                fishIdx2 = maps[fish_row_tmp][fish_col_tmp]
+                [fish_row2, fish_col2, fish_dir2] = fishStats_dict[fishIdx2]
+                fishStats_dict[fishIdx] = [fish_row_tmp, fish_col_tmp, fish_dir_tmp]
+                fishStats_dict[fishIdx2] = [fish_row, fish_col, fish_dir2]
+                maps[fish_row_tmp][fish_col_tmp] = fishIdx
+                maps[fish_row][fish_col] = fishIdx2
+                break
+            else:
+                print("moveFishes ERR!")
+
+    return maps, fishStats_dict
+
+def isSharkCanMove(maps, shark_row, shark_col, shark_dir):
+    flag = False
+    res = []
     for i in range(4):
-        shark_row_t = shark_row + dr[shark_dir_t]
-        shark_col_t = shark_col + dc[shark_dir_t]
-        if not isInMaps(shark_row_t, shark_col_t): break
-        if isFishIdx(maps, shark_row_t, shark_col_t):
-            fish_idx = maps[shark_row_t][shark_col_t]
-            maps, sharkStat = moveShark(maps, shark_row_t, shark_col_t, fishStats_dict, sharkStat)
-            playSharkDFS(maps.copy(), fishStats_dict.copy(), sharkStat, level+1, eat_sum+fish_idx)
-            leastOneFlag = True
-        # if leastOneFlag: break
+        shark_row_tmp = shark_row + DR[shark_dir]*i
+        shark_col_tmp = shark_col + DC[shark_dir]*i
+        if not isInMaps(shark_row_tmp, shark_col_tmp):
+            break
+        if isFish(maps, shark_row_tmp, shark_col_tmp):
+            flag = True
+            res.append(i)
+
+    return flag, res
 
 
-# 0-1. fishStats_dict / fish_idx: [row, col, dir]
-fishStats_dict = dict()
-for r in range(4):
-    for c in range(4):
-        fish_idx = maps[r][2*c+0]
-        fish_dir = maps[r][2*c+1]
-        fishStats_dict[fish_idx] = [r, c, fish_dir-1]
-fishStats_tmp = sorted(fishStats_dict.items())
-fishStats_dict = dict()
-for k, v in fishStats_tmp:
-    fishStats_dict[k] = v
-del(fishStats_tmp)
+# DFS
+def playSharkDFS(sharkStat, fishStats_dict, maps, level, eatIdxSum):
+    global MAX_EATIDXSUM
+    MAX_EATIDXSUM = max(eatIdxSum, MAX_EATIDXSUM)
 
-# 0-2. reset maps
-maps_legacy = maps.copy()
-maps = [[0]*4 for _ in range(4)]
-for r in range(4):
-    for c in range(4):
-        maps[r][c] = maps_legacy[r][2*c+0]
-del(maps_legacy)
+    #print(level, eatIdxSum, MAX_EATIDXSUM)
 
-# 1. sharkStat_list / [row, col, dir]
-sharkStat = []
-fish_idx = maps[0][0]
-[_, _, fish_dir] = fishStats_dict[fish_idx]
-del(fishStats_dict[fish_idx])
-sharkStat = [0, 0, fish_dir]
-maps[0][0] = -1
+    if not len(fishStats_dict) > 0: # if fish not exist
+        return
+    else:
+        moveFishes(maps, fishStats_dict)
 
-# main, DFS
-playSharkDFS(maps, fishStats_dict, sharkStat, 0, fish_idx)
+    [shark_row, shark_col, shark_dir] = sharkStat
+    sharkMoveFlag, sharkMoveIdxList = isSharkCanMove(maps, shark_row, shark_col, shark_dir)
+
+    if not sharkMoveFlag:
+        return
+    else:
+        for i in sharkMoveIdxList:
+            shark_row_tmp = shark_row + DR[shark_dir]*i
+            shark_col_tmp = shark_col + DC[shark_dir]*i
+
+            sharkStat_legacy        = copy.deepcopy(sharkStat)
+            maps_legacy             = copy.deepcopy(maps)
+            fishStats_dict_legacy   = copy.deepcopy(fishStats_dict)
+            eatIdxSum_legacy        = eatIdxSum
+
+            maps, sharkStat,fishStats_dict, eatIdxSum = sharkEatFish(sharkStat, shark_row_tmp, shark_col_tmp, maps, fishStats_dict, eatIdxSum)
+            playSharkDFS(sharkStat, fishStats_dict, maps, level+1, eatIdxSum)
+
+            sharkStat               = sharkStat_legacy
+            maps                    = maps_legacy
+            fishStats_dict          = fishStats_dict_legacy
+            eatIdxSum               = eatIdxSum_legacy
+            # restore!
+
+# MAIN
+playSharkDFS(sharkStat, fishStats_dict, maps, 0, eatIdxSum)
+print(MAX_EATIDXSUM)
