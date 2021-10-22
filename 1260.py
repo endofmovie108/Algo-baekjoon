4812b21b41525d0b6b4affec4064203860ed34d8
endofmovie108 <endofmovie108@gmail.com>
2021-10-23 02:37:39 +0900

    1260

--- 1260.py
+++ 1260.py
@@ -1,6 +1,8 @@
 import sys
 from collections import deque as dq
 
+# DFS 탐색에서, 이 문제에서는 방문 해제를 할 이유가 없음
+# dict 사용할때 key가 없을수 있으니 .get으로 None인지 체크 할 것!
 # 중요 반례
 # 5 5 5
 # 1 4
@@ -27,50 +29,43 @@ NOT_VISIT = 0
 # functions
 def appendSet(k, v):
     global connectInfo
-    if connectInfo.get(k) is None:
-        connectInfo[k] = set()
-    connectInfo[k].add(v)
+    if connectInfo.get(k-1) is None:
+        connectInfo[k-1] = set()
+    connectInfo[k-1].add(v-1)
 
-def DFS(lv):
-    global chk, connectInfo, cnt, res, isDone
-    if isDone:
-        return
-    if cnt == len(connectInfo.keys()):
-        isDone = True
-        return
-    cnt += 1
-    res.append(lv+1)
-    for next_v in connectInfo[lv+1]:
-        next_v -= 1
-        if chk[next_v] == NOT_VISIT:
-            chk[next_v] = VISIT
-            DFS(next_v)
-            chk[next_v] = NOT_VISIT
-
-def BFS(lv):
-    global chk
-    findDq = dq()
-    findDq.append([lv, 0])
-    chk[lv] = VISIT
-    while findDq:
-        [v, level] = findDq.popleft()
-
-        res.append(v+1)
-        if len(res) == len(connectInfo.keys()):
-            break
-        chk[v] = VISIT
-        for next_v in connectInfo[v+1]:
-            next_v -= 1
+def DFS(v):
+    global chk, res, v_tot_num, isDone
+    if isDone: return
+    chk[v] = VISIT
+    res.append(v)
+    tmp = connectInfo.get(v)
+    if tmp is not None:
+        for next_v in tmp:
             if chk[next_v] == NOT_VISIT:
-                findDq.append([next_v, level+1])
+                chk[next_v] = VISIT
+                DFS(next_v)
 
+def BFS(v):
+    global chk, res, v_tot_num
+    findDq = dq()
+    findDq.append(v)
+    chk[v] = VISIT
+    while findDq:
+        v = findDq.popleft()
+        res.append(v)
+        tmp = connectInfo.get(v)
+        if tmp is not None:
+            for next_v in tmp:
+                if chk[next_v] == NOT_VISIT:
+                    chk[next_v] = VISIT
+                    findDq.append(next_v)
 
 # MAIN
 for [v1, v2] in connects:
     appendSet(v1, v2)
     appendSet(v2, v1)
 
-for tmp_key in connectInfo:
+for tmp_key in connectInfo.keys():
     tmp_vals = list(connectInfo[tmp_key])
     connectInfo[tmp_key] = sorted(tmp_vals)
 
@@ -79,10 +74,12 @@ chk[V-1] = VISIT
 isDone, cnt = False, 0
 DFS(V-1)
 if res:
-    for r in res: print(r, end = " ")
+    for r in res: print(r+1, end = " ")
+
 res = []
 chk = [NOT_VISIT]*N
 BFS(V-1)
+
 if res:
     print()
-    for r in res: print(r, end = " ")
+    for r in res: print(r+1, end = " ")
